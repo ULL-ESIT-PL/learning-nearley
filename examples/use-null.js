4a280556a67e16ad5aa2e6cbdb5f8a8bd879dd13
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-05-22 09:24:28 +0100

    playing with null

--- examples/use-null.js
+++ examples/use-null.js
@@ -1,11 +1,16 @@
+/* 
+s -> "a":? 
+
+npx nearleyc null.ne  -o null.js
+npx nearley-test -i "a"  null.js  # [ [ [ 'a' ] ] ]
+npx nearley-test -i ''  null.js   # [ [ null ] ]
+node use-null.js
+*/
 const nearley = require("nearley");
 const grammar = require("./null.js");
 
-// Create a Parser object from our grammar.
 const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
 
-// Parse something!
 parser.feed(process.argv[2] || "");
 
-// parser.results is an array of possible parsings.
 //console.log(JSON.stringify(parser.results));
