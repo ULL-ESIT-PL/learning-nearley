453ef5147c0d84b9d2ecbe0320651e62c4145933
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-03-08 13:45:38 +0000

    catching-errors

--- examples/catching-errors/use-catching-errors.js
+++ examples/catching-errors/use-catching-errors.js
@@ -5,7 +5,7 @@ const grammar = require("./catching-errors.js");
 const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
 
 try {
-    parser.feed("Cow goes% moo.");
+    parser.feed(process.argv[2] || "Cow goes% moo.");
 } catch (parseError) {
     console.log("Error at character '" +parseError.token.value+"' at offset "+ parseError.offset); // "Error at character '%' at offset 8"
 }
